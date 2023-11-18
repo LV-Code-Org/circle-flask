cd057d0c92219a6443ac146391caade77e634fd8
Vihaan Sinha <vihaansinha@Vihaans-MacBook-Air.local>
2023-11-18 12:32:37 -0500

    a2hosting update

---
+++ passenger_wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+import importlib.util
+
+# Required for A2 Hosting
+
+sys.path.insert(0, os.path.dirname(os.path.realpath('__file__')))
+
+spec = importlib.util.spec_from_file_location("wsgi", "app.py")
+wsgi = importlib.util.module_from_spec(spec)
+spec.loader.exec_module(wsgi)
+application = wsgi.app
